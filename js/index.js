77d6c0f99e738527723865f2a5d1728434106445
Rusbel Bermudez <36142887+rb-one@users.noreply.github.com>
2022-07-07 23:26:24 -0500

    version inicial invitacion nico

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let audio = new Audio('../audio/t-rex.mp3');
+
+window.addEventListener("scroll", (event) => {
+    let trexContainer = document.getElementById("t-rex-Container");
+    let scroll = this.scrollY;
+
+    if (scroll >= 1300) {
+        let trex = document.getElementById("t-rex");
+        if (trex === null) {
+            let img = document.createElement('img');
+            img.className = 'logo-nico-3'
+            img.src = "../images/nico-logo-3.gif"
+            img.id = "t-rex"
+            trexContainer.appendChild(img)
+        }
+    }
+
+
+    if (scroll >= 1800) {
+        audio.play();
+    }
+
+    if (scroll <= 1300) {
+        let trex = document.getElementById("t-rex");
+        if (trex !== null) {
+            audio.pause();
+            audio.currentTime = 0;
+            trex.remove();
+
+        }
+    }
+
+
+});
